d9c55f16e9f311187bbeea36d66affcccac83c5f
koa <jshawty82@gmail.com>
2015-11-10 00:55:00 -0500

    Added unfinished faq page

--- web/app/app.js
+++ web/app/app.js
@@ -1,12 +1,15 @@
 var origin = document.location.origin;
 var folder = document.location.pathname.split('/')[1];
-var path = origin + '/' + folder + '/';
+var path = origin + '/' + folder + '/coursegrab/web/';
 
 var courseGrab = angular.module('courseGrab', ['ngRoute']);
 courseGrab.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/search/:page?', {
             templateUrl: path + 'views/courses.html',
             controller: 'courseController'
+        }).when('/faq', {
+            templateUrl: path + 'views/faq.html',
+            controller: function() {}
         })
         .otherwise({redirectTo: '/search'});
 
